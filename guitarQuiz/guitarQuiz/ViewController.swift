ab4c42adb2f690e5e91eaa1307ceb363063e82a8
Mayur Nimba Beldar <mbeldar@okstate.edu>
2017-09-05 18:21:12 -0500

    Initial
    Intiatial commit

---
+++ guitarQuiz/guitarQuiz/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  guitarQuiz
+//
+//  Created by MayurBeldar on 9/5/17.
+//  Copyright © 2017 MayurBeldar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func Gestured(_ sender: UIPanGestureRecognizer) {
+        print("ff")
+    }
+    
+}
+
